fe171e81034a4235f945ede4009e01886d17d369
Muhammed Mahmood Alam <muhammed.alam.ext@sivantos.com>
2022-12-22 18:23:56 +0530

    initial commit

---
+++ src/components/Text/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+const variantClasses = {
+  h1: "font-bold sm:text-[38px] md:text-[44px] text-[48px]",
+  h2: "font-normal sm:text-[28px] md:text-[30px] text-[32px]",
+  h3: "font-normal sm:text-[20px] md:text-[22px] text-[24px]",
+  h4: "font-normal text-[20px]",
+  h5: "font-normal text-[16px]",
+  h6: "text-[14px]",
+  body1: "font-normal text-[10px]",
+};
+const Text = ({ children, className, variant, as, ...restProps }) => {
+  const Component = as || "span";
+  return (
+    <Component
+      className={`${className} ${variantClasses[variant]}`}
+      {...restProps}
+    >
+      {children}
+    </Component>
+  );
+};
+
+export { Text };
